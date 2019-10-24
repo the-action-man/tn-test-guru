bfee14dd85e43081e83e2b4c3182a07ce6b4f4fa
Andrei <the.action.man@yandex.com>
2019-10-25 00:56:14 +0300

    migration AddUserRefToTests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_202546) do
+ActiveRecord::Schema.define(version: 2019_10_24_213718) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text", null: false
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_10_24_202546) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "level", default: 0
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
