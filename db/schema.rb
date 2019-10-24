d500ded471e615c3437a0f48887207937afd3c97
Andrei <the.action.man@yandex.com>
2019-10-24 23:27:54 +0300

    class AddAnswersCorrectNullConstraint < ActiveRecord::Migration[5.2]

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_193650) do
+ActiveRecord::Schema.define(version: 2019_10_24_202546) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text", null: false
     t.integer "question_id", null: false
-    t.boolean "correct", default: false
+    t.boolean "correct", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
