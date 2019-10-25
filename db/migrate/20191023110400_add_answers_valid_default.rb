bdca5712c5a554d0af66bf57179441dcb4ad2df7
Andrei <the.action.man@yandex.com>
2019-10-25 21:38:12 +0300

    fix using from: to: in change_column_default

--- db/migrate/20191023110400_add_answers_valid_default.rb
+++ db/migrate/20191023110400_add_answers_valid_default.rb
@@ -1,5 +1,5 @@
 class AddAnswersValidDefault < ActiveRecord::Migration[5.2]
   def change
-    change_column_default(:answers, :valid, false)
+    change_column_default(:answers, :valid, from: nil, to: false)
   end
 end
