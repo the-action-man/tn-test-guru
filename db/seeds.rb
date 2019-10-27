9577e7ee9c162bf363bbdb28b346f3f5fb0ee663
Andrei <the.action.man@yandex.com>
2019-10-27 17:17:36 +0300

    seeds improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,59 +8,78 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create([
-              { name: 'user_01' },
-              { name: 'user_02' },
-              { name: 'user_03' }
-            ])
-Category.create([
-                  { name: 'category_01' },
-                  { name: 'category_02' },
-                  { name: 'category_03' }
-                ])
-Test.create([
-              { name: 'test_01', level: 0, user_id: 1, category_id: 1 },
-              { name: 'test_01', level: 1, user_id: 1, category_id: 1 },
-              { name: 'test_01', level: 2, user_id: 1, category_id: 1 },
-              { name: 'test_02', level: 2, user_id: 1, category_id: 1 },
-              { name: 'test_03', level: 2, user_id: 1, category_id: 1 },
-              { name: 'test_04', level: 1, user_id: 2, category_id: 2 },
-              { name: 'test_05', level: 0, user_id: 3, category_id: 3 }
-            ])
-Question.create([
-                  { text: 'question_01', test_id: 1 },
-                  { text: 'question_02', test_id: 1 },
-                  { text: 'question_03', test_id: 1 },
-                  { text: 'question_04', test_id: 1 },
-                  { text: 'question_05', test_id: 1 },
-                  { text: 'question_06', test_id: 1 },
-                  { text: 'question_07', test_id: 1 },
-                  { text: 'question_08', test_id: 1 },
-                  { text: 'question_09', test_id: 1 },
-                  { text: 'question_10', test_id: 1 },
-                  { text: 'question_201', test_id: 2 },
-                  { text: 'question_202', test_id: 2 },
-                  { text: 'question_203', test_id: 2 },
-                  { text: 'question_204', test_id: 2 },
-                  { text: 'question_205', test_id: 2 },
-                  { text: 'question_206', test_id: 2 },
-                  { text: 'question_207', test_id: 2 },
-                  { text: 'question_208', test_id: 2 },
-                  { text: 'question_209', test_id: 2 },
-                  { text: 'question_210', test_id: 2 },
-                  { text: 'question_301', test_id: 3 }
-                ])
-Answer.create([
-                { text: 'a_01 of q_01', question_id: 1, correct: false },
-                { text: 'a_01 of q_01', question_id: 1, correct: false },
-                { text: 'a_01 of q_01', question_id: 1, correct: false },
-                { text: 'a_01 of q_01 true', question_id: 1, correct: true },
-                { text: 'a_01 of q_02', question_id: 2, correct: false },
-                { text: 'a_01 of q_02', question_id: 2, correct: false },
-                { text: 'a_01 of q_02', question_id: 2, correct: false },
-                { text: 'a_01 of q_02 true', question_id: 2, correct: true },
-                { text: 'a_01 of q_03', question_id: 3, correct: false },
-                { text: 'a_01 of q_03', question_id: 3, correct: false },
-                { text: 'a_01 of q_03', question_id: 3, correct: false },
-                { text: 'a_01 of q_03 true', question_id: 3, correct: true }
-              ])
+users = User.create!([
+                       { name: 'user_01' },
+                       { name: 'user_02' },
+                       { name: 'user_03' }
+                     ])
+categories = Category.create!([
+                                { name: 'category_01' },
+                                { name: 'category_02' },
+                                { name: 'category_03' }
+                              ])
+tests = Test.create!([
+                       { name: 'test_01', level: 0, user_id: users[0].id, \
+                         category_id: categories[0].id },
+                       { name: 'test_01', level: 1, user_id: users[0].id, \
+                         category_id: categories[0].id },
+                       { name: 'test_01', level: 2, user_id: users[0].id, \
+                         category_id: categories[0].id },
+                       { name: 'test_02', level: 2, user_id: users[0].id, \
+                         category_id: categories[0].id },
+                       { name: 'test_03', level: 2, user_id: users[0].id, \
+                         category_id: categories[0].id },
+                       { name: 'test_04', level: 1, user_id: users[1].id, \
+                         category_id: categories[1].id },
+                       { name: 'test_05', level: 0, user_id: users[2].id, \
+                         category_id: categories[2].id }
+                     ])
+questions = Question.create!([
+                               { text: 'question_01', test_id: tests[0].id },
+                               { text: 'question_02', test_id: tests[0].id },
+                               { text: 'question_03', test_id: tests[0].id },
+                               { text: 'question_04', test_id: tests[0].id },
+                               { text: 'question_05', test_id: tests[0].id },
+                               { text: 'question_06', test_id: tests[0].id },
+                               { text: 'question_07', test_id: tests[0].id },
+                               { text: 'question_08', test_id: tests[0].id },
+                               { text: 'question_09', test_id: tests[0].id },
+                               { text: 'question_10', test_id: tests[0].id },
+                               { text: 'question_201', test_id: tests[1].id },
+                               { text: 'question_202', test_id: tests[1].id },
+                               { text: 'question_203', test_id: tests[1].id },
+                               { text: 'question_204', test_id: tests[1].id },
+                               { text: 'question_205', test_id: tests[1].id },
+                               { text: 'question_206', test_id: tests[1].id },
+                               { text: 'question_207', test_id: tests[1].id },
+                               { text: 'question_208', test_id: tests[1].id },
+                               { text: 'question_209', test_id: tests[1].id },
+                               { text: 'question_210', test_id: tests[1].id },
+                               { text: 'question_301', test_id: tests[2].id }
+                             ])
+answers = Answer.create!([
+                           { text: 'a_01 of q_01', \
+                             question_id: questions[0].id, correct: false },
+                           { text: 'a_01 of q_01', \
+                             question_id: questions[0].id, correct: false },
+                           { text: 'a_01 of q_01', \
+                             question_id: questions[0].id, correct: false },
+                           { text: 'a_01 of q_01 true', \
+                             question_id: questions[0].id, correct: true },
+                           { text: 'a_01 of q_02', \
+                             question_id: questions[1].id, correct: false },
+                           { text: 'a_01 of q_02', \
+                             question_id: questions[1].id, correct: false },
+                           { text: 'a_01 of q_02', \
+                             question_id: questions[1].id, correct: false },
+                           { text: 'a_01 of q_02 true', \
+                             question_id: questions[1].id, correct: true },
+                           { text: 'a_01 of q_03', \
+                             question_id: questions[2].id, correct: false },
+                           { text: 'a_01 of q_03', \
+                             question_id: questions[2].id, correct: false },
+                           { text: 'a_01 of q_03', \
+                             question_id: questions[2].id, correct: false },
+                           { text: 'a_01 of q_03 true', \
+                             question_id: questions[2].id, correct: true }
+                         ])
