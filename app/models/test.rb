88e4a28db5b814c9379705992c15e87e0e0f4fa4
Andrei <the.action.man@yandex.com>
2019-10-30 22:30:18 +0300

    add associations for answers, questions, categories, tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,7 @@
 class Test < ApplicationRecord
+  has_many :questions
+  belongs_to :category
+
   def self.by_category(category)
     joins('INNER JOIN categories ON tests.category_id = categories.id')
       .where(categories: { name: category })
