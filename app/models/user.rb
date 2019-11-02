04b0a4d2b787d8f2ec8fdee683d9ae705778c30c
Andrei (the.action.man) <49880822+the-action-man@users.noreply.github.com>
2019-11-02 19:34:49 +0300

    update app/models/user.rb
    Co-Authored-By: max-underthesun <max.underthesun@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :creations, class_name: "Test", foreign_key: "author_id"
 
   def tests_with_level(level)
-    tests.where(tests: { level: level })
+    tests.where(level: level)
   end
 end
