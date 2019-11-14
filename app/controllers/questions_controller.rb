4944c276cd6b8bb11b7c51dd06127f7097e8d0aa
Andrei <the.action.man@yandex.com>
2019-11-14 13:33:11 +0300

    add find_question to question_controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -7,9 +8,7 @@ class QuestionsController < ApplicationController
     render json: @test.questions
   end
 
-  def show
-    @question = Question.find(params[:id])
-  end
+  def show; end
 
   def new; end
 
@@ -23,14 +22,17 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    question = Question.find(params.permit(:id))
-    question.destroy
+    @question.destroy
   end
 
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_question_not_found
