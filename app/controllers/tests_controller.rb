2a2bfe582a77f5a7f90815d33c4c74aafcbc6ed7
Andrei <the.action.man@yandex.com>
2019-11-18 01:43:45 +0300

    add test creation

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,11 +1,45 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show destroy]
 
   def index
     @tests = Test.all
   end
 
-  def show
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  private
+
+  def find_test
     @test = Test.find(params[:id])
-    @questions = @test.questions
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 end
