92304e16092e39e840fec7b9eba806ce9deec2ea
Andrei <the.action.man@yandex.com>
2019-11-19 16:43:48 +0300

    optimization question_header

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,12 +1,6 @@
 module QuestionsHelper
-  def question_header
-    if @question.new_record?
-      action_name = 'Create'
-      test_title = @test.title
-    else
-      action_name = 'Edit'
-      test_title = @question.test.title
-    end
-    "#{action_name} question for test '#{test_title}'"
+  def question_header(question)
+    action_name = question.new_record? ? 'Create' : 'Edit'
+    "#{action_name} question for test '#{question.test.title}'"
   end
 end
