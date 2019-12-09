0b079cead0b32b1d080f334fa31cfa58d43b619c
Andrei <the.action.man@yandex.com>
2019-12-09 23:42:16 +0300

    remove module User::Auth and add RoR method has_secure_password into User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
-  include Auth
-  # has_secure_password # do something like my module Auth
-
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :creations, class_name: "Test", foreign_key: "author_id", dependent: :destroy
+
+  has_secure_password
 
   def tests_with_level(level)
     tests.with_level(level)
