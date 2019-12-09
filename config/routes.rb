dda401b8cc0f42150f4ffcdef6ba20d9e3504cae
Andrei <the.action.man@yandex.com>
2019-12-09 23:16:27 +0300

    add feature authentification

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'users/new'
+
   root to: 'tests#index'
+
+  get :signup, to: 'users#new'
+  resources :users, only: %i[create]
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
