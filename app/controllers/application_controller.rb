ac32e2ac69d63036058f985a04ad18b775de708c
Andrei <the.action.man@yandex.com>
2019-12-12 13:57:33 +0300

    add navbar and email cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,10 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user!
     unless current_user
-      redirect_to login_path, alert: 'Are you Guru? Verify your Email and Password, please'
+      return redirect_to login_path, alert: 'Are you Guru? Verify your Email and Password, please'
     end
+
+    cookies[:email] = current_user.email
   end
 
   def current_user
