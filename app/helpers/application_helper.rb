a3a355dcb69bcf7b71a76f3772547728f4af18d3
Andrei <the.action.man@yandex.com>
2019-12-14 19:16:01 +0300

    refactor flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,11 @@
 module ApplicationHelper
+  FLASH_MESSAGES_KEYS = %i[success notice alert].freeze
+
   def current_year
     Time.current.year
   end
 
   def github_url(author, repo)
     "https://github.com/#{author}/#{repo}"
-  end
-
-  def flash_message_2
-    case flash.keys[0]
-    when 'notice'
-      flash_type = 'notice'
-    when 'success'
-      flash_type = 'success'
-    when 'alert'
-      flash_type = 'alert'
-    else
-      return
-    end
-    content_tag :p, flash[flash_type], class: "flash #{flash_type}"
-  end
-
-  def flash_message
-    flash_type = flash.keys[0] || return
-
-    content_tag :p, flash[flash_type], class: "flash #{flash_type}"
   end
 end
