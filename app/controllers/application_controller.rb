777e430ee9a86573e1338b44fb4bf824782308a1
Andrei <the.action.man@yandex.com>
2019-12-24 14:12:37 +0300

    add redirect to admin/tests for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+      if resource.is_a?(Admin)
+        admin_tests_path
+      else
+        super
+      end
+  end
 end
