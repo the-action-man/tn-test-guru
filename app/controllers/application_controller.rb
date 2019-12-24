a675367fcc7667913cddc02ac2dc71f75dc1528c
Andrei <the.action.man@yandex.com>
2019-12-24 18:03:02 +0300

    add show first_name and last_name in admin panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) ||
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
       else
         super
       end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
   end
 end
