91f9584dd7a8f4f754ba38e5184a97b818bafd07
Andrei <the.action.man@yandex.com>
2019-12-24 13:41:45 +0300

    add namespace admin with appropriate controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
-  resources :tests do
+  resources :tests, only: :index do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: %i[index show]
     end
@@ -20,4 +20,9 @@ Rails.application.routes.draw do
       get :result
     end
   end
+
+  namespace :admin do
+    resources :tests
+  end
+
 end
