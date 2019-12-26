881b00f653020bde9528b9d2b876fbfca1d0eb6b
Andrei <the.action.man@yandex.com>
2019-12-26 23:46:20 +0300

    refactor devise login and admin features

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -7,6 +7,6 @@ class Admin::BaseController < ApplicationController
 
   def admin_required!
     redirect_to root_path, alert: 'You are not authorized to view this page.' \
-                                                                    unless current_user.is_a?(Admin)
+                                                                    unless current_user.admin?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages
+  has_many :creations, class_name: "Test", foreign_key: "author_id", dependent: :destroy
+
   devise :database_authenticatable,
          :registerable,
          :recoverable,
@@ -7,19 +11,19 @@ class User < ApplicationRecord
          :validatable,
          :confirmable
 
-  has_many :test_passages, dependent: :destroy
-  has_many :tests, through: :test_passages
-  has_many :creations, class_name: "Test", foreign_key: "author_id", dependent: :destroy
-
-  validates :email, presence: true,
-                    uniqueness: true,
-                    format: { with: /.+@.+/i }
-
   def tests_with_level(level)
     tests.with_level(level)
   end
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
+
+  def admin?
+    type == 'Admin'
+  end
+
+  def user?
+    type == 'User'
   end
 end
