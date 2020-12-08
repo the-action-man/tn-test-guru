fc12be018600a1e75276bd4d4bad185b960e30ab
Andrei <the.action.man@yandex.com>
2020-12-08 22:25:05 +0300

    1) using let 2) add linter check

--- app/assets/javascripts/table/sorting.js
+++ app/assets/javascripts/table/sorting.js
@@ -1,61 +1,61 @@
-document.addEventListener('turbolinks:load', function() {
-    var control = document.querySelector('.sort-by-title-js');
+document.addEventListener('turbolinks:load', function () {
+  let titleColumnHeader = document.querySelector('.sort-by-title-js')
 
-    if(control) { control.addEventListener('click', sortRowsByTitle); }
-});
+  if (titleColumnHeader) titleColumnHeader.addEventListener('click', sortRowsByTitle)
+})
 
-function sortRowsByTitle() {
-    var table = document.querySelector('.table');
+function sortRowsByTitle () {
+  let table = document.querySelector('.table')
 
-    var sortedRows = selectTableRows(table);
-    handleSorting(sortedRows);
+  let sortedRows = selectTableRows(table)
+  handleSorting(sortedRows)
 
-    var sortedTable = createSortedTableElement(selectTableHeader(table), sortedRows);
-    table.parentNode.replaceChild(sortedTable, table);
+  let sortedTable = createSortedTableElement(selectTableHeader(table), sortedRows)
+  table.parentNode.replaceChild(sortedTable, table)
 }
 
-function handleSorting(sortedRows) {
-    if (title_column_header.getAttribute("data-sort-state") === "desc") {
-        sortedRows.sort((row1, row2) => row1.cells[0].innerHTML > row2.cells[0].innerHTML ? 1 : -1);
-        octicon_title_arrow_up.classList.remove('hide');
-        octicon_title_arrow_down.classList.add('hide');
-        title_column_header.setAttribute("data-sort-state", "asc")
-    } else {
-        sortedRows.sort((row1, row2) => row1.cells[0].innerHTML < row2.cells[0].innerHTML ? 1 : -1);
-        octicon_title_arrow_up.classList.add('hide');
-        octicon_title_arrow_down.classList.remove('hide');
-        title_column_header.setAttribute("data-sort-state", "desc")
-    }
+function handleSorting (sortedRows) {
+  if (title_column_header.getAttribute('data-sort-state') === 'desc') {
+    sortedRows.sort((row1, row2) => row1.cells[0].innerHTML > row2.cells[0].innerHTML ? 1 : -1)
+    octicon_title_arrow_up.classList.remove('hide')
+    octicon_title_arrow_down.classList.add('hide')
+    title_column_header.setAttribute('data-sort-state', 'asc')
+  } else {
+    sortedRows.sort((row1, row2) => row1.cells[0].innerHTML < row2.cells[0].innerHTML ? 1 : -1)
+    octicon_title_arrow_up.classList.add('hide')
+    octicon_title_arrow_down.classList.remove('hide')
+    title_column_header.setAttribute('data-sort-state', 'desc')
+  }
 }
 
-function createSortedTableElement(tableHeaderRow, sortedRows) {
-    var sortedTable = document.createElement('table');
-    sortedTable.classList.add('table');
-    sortedTable.appendChild(tableHeaderRow);
+function createSortedTableElement (tableHeaderRow, sortedRows) {
+  let sortedTable = document.createElement('table')
+  sortedTable.classList.add('table')
+  sortedTable.appendChild(tableHeaderRow)
 
-    for (var i = 0; i < sortedRows.length; i++) {
-        sortedTable.appendChild(sortedRows[i])
-    }
+  for (let i = 0; i < sortedRows.length; i++) {
+    sortedTable.appendChild(sortedRows[i])
+  }
 
-    return sortedTable;
+  return sortedTable
 }
 
-function selectTableRows(table) {
-    var rows = selectAllTableRowsElements(table);
-    var selectedRows = [];
+function selectTableRows (table) {
+  let rows = selectAllTableRowsElements(table)
+  let selectedRows = []
 
-    // select all table rows except the first one which is the header
-    for (var i = 1; i < rows.length; i++) {
-        selectedRows.push(rows[i]);
-    }
+  // select all table rows except the first one which is the header
+  for (let i = 1; i < rows.length; i++) {
+    selectedRows.push(rows[i])
+  }
 
-    return selectedRows;
+  return selectedRows
 }
 
-function selectTableHeader(table) {
-    return selectAllTableRowsElements(table)[0];
+function selectTableHeader (table) {
+  return selectAllTableRowsElements(table)[0]
 }
 
-function selectAllTableRowsElements(table) {
-    return table.querySelectorAll('tr');
+function selectAllTableRowsElements (table) {
+  return table.querySelectorAll('tr')
 }
