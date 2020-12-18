116d318601600ed9a269c1966a5ed585a5847dec
Andrei <the.action.man@yandex.com>
2020-12-18 21:27:53 +0300

    [part_17_deploy] fix heroku glitch: ERROR:  column "test_id" of relation "answers" does not exist
    (cherry picked from commit 5d17bbf8d7a90f4e452ac22bbb43497b9cf7f3c7)

--- db/migrate/20191024220449_remove_test_from_answers.rb
+++ db/migrate/20191024220449_remove_test_from_answers.rb
@@ -1,5 +1,5 @@
 class RemoveTestFromAnswers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :answers, :test_id, :integer
+    # remove_column :answers, :test_id, :integer
   end
 end
