a839392a861e602ee9a3885dfdc0a9f9ea3bea5e
Andrei <the.action.man@yandex.com>
2020-12-19 23:33:08 +0300

    [part_17_deploy] implement closable flash to see navigation bar

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,6 +1,6 @@
 module FlashHelper
   def flash_class(flash_type)
     alert_class = { alert: "alert-danger", notice: "alert-info" }[flash_type.to_sym]
-    "alert #{alert_class}"
+    "text-center w-75 p-3 alert #{alert_class} alert-dismissible fade show"
   end
 end
