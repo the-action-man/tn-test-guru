a56b70e8ba366f1500ddac0dd6c7192d924b8fc5
Andrei <the.action.man@yandex.com>
2020-12-20 21:29:14 +0300

    [part_17_deploy] add using URI::MailTo::EMAIL_REGEXP to verify email format

--- app/models/support_message.rb
+++ app/models/support_message.rb
@@ -1,7 +1,6 @@
 class SupportMessage < ApplicationRecord
   validates :email, :message, presence: true
   validates :email,
-            format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
-                      message: "format is invalid" },
+            format: { with: URI::MailTo::EMAIL_REGEXP, message: "format is invalid" },
             length: { minimum: 5, maximum: 254 }
 end
